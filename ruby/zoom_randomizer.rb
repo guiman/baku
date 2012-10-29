05a99914390e307fe008122ac0cb79e8bdfcd193
Alvaro F. Lara <alvarola@gmail.com>
2012-10-29 00:35:26 -0300

    Fixing the zoom randomizer. Now it behaves as expected

--- ruby/zoom_randomizer.rb
+++ ruby/zoom_randomizer.rb
@@ -1,29 +1,30 @@
 require "./randomizer"
 
 # This is a specialized Randomizer that simulates two points get closer/far away from each other
-# 
 class ZoomRandomizer < Randomizer
   
   def initialize(how_far_away = 100)
-    @x1 = 0
-    @x2 = how_far_away
-    @y1 = 0
-    @y2 = how_far_away
-    @factor_p_1 = 1
-    @factor_p_2 = -1
+    @p1 = 0
+    @p2 = how_far_away
+    @how_far = how_far_away
+    @factor = -1
   end
   
+  def switch_factor
+    @factor = @factor * -1
+  end 
+  
   def run
-    if (@x1 == @x2)
-      @factor_p_1 * -1
-      @factor_p_2 * -1
+    if @factor < 0
+      @p1 +=  1
+      @p2 -=  1
+    else
+      @p1 -=  1
+      @p2 +=  1
     end
+
+    switch_factor if (@p1 == @p2) || (@p1 == 0 || @p2 == @how_far)
     
-    @x1 +=  1 * @factor_p_1
-    @y1 +=  1 * @factor_p_1
-    @x2 +=  1 * @factor_p_2
-    @y2 +=  1 * @factor_p_2
-    
-    [[@x1, @y1], [@x2, @y2]]
+    [[@p1, @p1], [@p2, @p2]]
   end
 end
